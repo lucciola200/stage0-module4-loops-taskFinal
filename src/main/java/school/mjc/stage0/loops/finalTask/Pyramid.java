8d73dd8121cb55480aa16808e029c2984e865ebc
lucciolla200 <teoteo.odisharia@gmail.com>
2022-08-13 18:01:38 +0400

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength + i; j++) {
+                if (j == cathetusLength) {
+                    System.out.print(1);
+                } else if (j >= cathetusLength - i && j <= cathetusLength + i) {
+                    if (j >= cathetusLength) {
+                        System.out.print(j - cathetusLength + 1);
+                    } else if (j <= cathetusLength + i) {
+                        System.out.print(cathetusLength - j + 1);
+                    }
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
